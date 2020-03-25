80557bda22ffe77df4b98ced5075eb43c5d7f4ac
kafka <kafka@GregorSamsa>
2020-03-25 03:51:57 -0300

    Classe de persistência de Venda criada

---
+++ VendaDeTrofeus/src/main/java/com/vulture/start/dao/VendaRepository.java
@@ -0,0 +1,9 @@
+package com.vulture.start.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vulture.start.model.Venda;
+
+public interface VendaRepository extends JpaRepository<Venda, Integer>{
+
+}
