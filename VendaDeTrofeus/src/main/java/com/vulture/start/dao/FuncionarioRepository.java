525834aa30cf9a3ec250a1b464bde81cdebfa554
Jobson <kafka@GregorSamsa>
2020-04-01 22:21:19 -0300

    O Controller, Repository e o Model da classe Funcionario foram excluídas

--- VendaDeTrofeus/src/main/java/com/vulture/start/dao/FuncionarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vulture.start.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.vulture.start.model.Funcionario;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer>{
-
-}
